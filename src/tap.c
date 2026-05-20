c595ec69e875f34cc36c3fc2f3aa508350a37e9f
duncan <duncand9199@gmail.com>
2026-05-19 21:12:43 -0400

    Added more comments to tap.c

--- src/tap.c
+++ src/tap.c
@@ -2,6 +2,12 @@
  * tap.c
  *
  * Handles Linux TAP device interaction
+ *
+ * TUN and TAP are kernel virtual network devices within Linux environments.
+ * TUN carries IP packets, TAP carries Ethernet frames.
+ *
+ * Linux exposes TAP devices are file descriptors, which means we can use 
+ * things like read() and write() on frames the same way they work on files. 
  */
 
 #include <stdio.h>
@@ -9,21 +15,54 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+// For some device-specific operations
 #include <sys/ioctl.h>
 
+// Linux network interface structures
 #include <linux/if.h>
+
+// TUN/TAP-specific constants and definitions
 #include <linux/if_tun.h>
 
 #include "tap.h"
 
 /*
  * Open or create TAP interface
+ *
+ * tap_open expects a character array (ex. "tap0")
+ * but we pass it as a pointer to a string since the kernel may modify it
+ *
+ * For instance, if the user requests "tap%d", the kernel may return "tap0"
+ *
+ * So the function may write back to this memory
  */
 int tap_open(char *devname) {
+
+	/*
+	 * Linux kernel structure used for network interface configuration
+	 * if = interface
+	 * req = request
+	 */
 	struct ifreq *ifr;
 
 	
-	// Open TUN/TAP clone device
+	/*
+	 * open() is a standard Unix syscall. Opens a file/device and returns 
+	 * a file descriptor
+	 *
+	 * 0 -> stdin
+	 * 1 -> stdout
+	 * 2 -> stderr
+	 *
+	 * open() can also return values greater than 2, meaning 
+	 * "resource <integer>"
+	 *
+	 * /dev/net/tun is the Linux kernel device for TUN/TAP 
+	 * functionality.
+	 *
+	 * O_RDWR = Open for reading and writing (we want both because 
+	 * eventually we will want to receive AND send frames)
+	 */
 	int fd = open("/dev/net/tun", O_RDWR);
 
 	if (fd < 0) {
