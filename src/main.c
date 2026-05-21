3163085679cfb5db2049fb1d0a121b83f13dd9fd
duncan <duncand9199@gmail.com>
2026-05-20 22:34:01 -0400

    First main.c draft

---
+++ src/main.c
@@ -0,0 +1,57 @@
+/*
+ * main.c
+ *
+ * Current goal functionality is to open a TAP interface, receive raw 
+ * Ethernet frames, print frame info, and hexdump frame contents
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "tap.h"
+#include "util.h"
+#include "ethernet.h"
+
+int main(void) {
+	// TAP interface name. Kernel may overwrite this if needed
+	char tap_name[IFNAMSIZ] = "tap0";
+
+	// Open TAP interface
+	int tap_fd = tap_open(tap_name);
+
+	if (tap_fd < 0) {
+		fprintf(stderr, "Failed to open TAP interface\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("[+] Opened TAP interface: %s\n", tap_name);
+
+	// Packet receive buffer
+	uint8_t buffer[TAP_BUFFER_SIZE];
+
+	// Main receive loop
+	while (1) {
+
+		ssize_t bytes_read = 
+			tap_read(tap_fd, buffer, sizeof(buffer));
+
+		if (bytes_read < 0) {
+			perror("tap_read");
+			continue;
+		}
+
+		printf("\n[+] Received Ethernet frame (%zd bytes)\n",
+				bytes_read);
+
+		// Parse/display Ethernet header
+		ethernet_handle_frame(buffer, bytes_read);
+
+		// Print raw packet bytes
+		printf("\nHex Dump:\n");
+
+		hex_dump(buffer, bytes_read);
+	}
+
+	return EXIT_SUCCESS;
+}
